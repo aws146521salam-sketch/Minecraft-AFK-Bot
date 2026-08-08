2c9382d3a87130377b403a6d262086f337aa1369
aws146521salam-sketch <aws146521salam@gmail.com>
2026-08-08 17:31:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,43 @@
 const bedrock = require('bedrock-protocol');
+const dns = require('dns');
 const config = require('./config.json');
 
 function connectBot() {
-  console.log('البوت يحاول الاتصال بسيرفر البدروك...');
+  console.log(`جاري تحليل عنوان السيرفر: ${config.serverHost}...`);
   
-  const client = bedrock.createClient({
-    host: config.serverHost,
-    port: config.serverPort,
-    username: config.botUsername,
-    offline: true,
-    skipPing: true
-  });
+  dns.lookup(config.serverHost, (err, address) => {
+    if (err) {
+      console.log('خطأ في العثور على IP السيرفر، إعادة المحاولة بعد 10 ثواني...', err);
+      setTimeout(connectBot, 10000);
+      return;
+    }
 
-  client.on('connect', () => {
-    console.log('تم الاتصال الأولي بالسيرفر!');
-  });
+    console.log(`تم العثور على الـ IP: ${address}, جاري الاتصال...`);
 
-  client.on('spawn', () => {
-    console.log('تم الاتصال! البوت الآن موجود داخل السيرفر.');
-  });
+    const client = bedrock.createClient({
+      host: address,
+      port: config.serverPort,
+      username: config.botUsername,
+      offline: true,
+      skipPing: true
+    });
 
-  client.on('error', (err) => {
-    console.log('خطأ في الاتصال:', err);
-  });
+    client.on('connect', () => {
+      console.log('تم الاتصال الأولي بالسيرفر!');
+    });
 
-  client.on('close', () => {
-    console.log('انقطع الاتصال، سيتم اعادة المحاولة بعد 10 ثواني...');
-    setTimeout(connectBot, 10000);
+    client.on('spawn', () => {
+      console.log('تم الاتصال! البوت الآن موجود داخل السيرفر.');
+    });
+
+    client.on('error', (err) => {
+      console.log('خطأ في الاتصال:', err);
+    });
+
+    client.on('close', () => {
+      console.log('انقطع الاتصال، سيتم اعادة المحاولة بعد 10 ثواني...');
+      setTimeout(connectBot, 10000);
+    });
   });
 }
 
